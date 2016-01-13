59c9e7c3e2d31fc82c3ee6c3cd533b2eac7b72fb
Enrique Bondoc <enrique.bondoc@writetospeak.info>
2016-01-13 18:56:55 +0800

    Added Home Page Description
    Home Page description list

--- views/home/index.php
+++ views/home/index.php
@@ -9,7 +9,36 @@
             <?php require 'views/shared/header.php'; ?>            
             <div class='content'>
                 <h1>Academetrics</h1>
-                <h3>Changelog!</h3>
+                <h3>What is Academetrics?</h3>
+                <ul>
+                    <li>
+                        <strong>Academetrics</strong> is an academic management tool.
+                    </li>
+                    <li>
+                        Students and Administrators can log in to manage their accounts.
+                    </li>
+                    <li>
+                        Students can be added by Administrators.
+                    </li>
+                    <li>
+                        Students can manage their own subjects.
+                    </li>
+                    <li>
+                        Professors/Lecturers can be added at a later time (next release).
+                    </li>
+                </ul>
+                <h3>About Academetrics</h3>
+                <ul>
+                    <li>
+                        <strong>Academetrics</strong> use the same basic framework as the blog I use on writetospeak.
+                    </li>
+                    <li>
+                        OOP is something I don't opt to use if I want to go full ham for performance, so you should not be able to see any OOP tricks on that basic framework.
+                    </li>
+                    <li>
+                        Code is in <a href='https://github.com/redspade-redspade/academetrics'>Github for review</a>, but I locked it out just so no one could tamper with my own code base.
+                    </li>
+                </ul>
             </div>
         </div>
     </body>
